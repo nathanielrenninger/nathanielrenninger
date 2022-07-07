30f51e06a2d6690230a2ca5b4fd8400d11b8134d
nathanielrenninger <108883707+nathanielrenninger@users.noreply.github.com>
2022-07-07 15:56:43 -0400

    update

---
+++ property-management/Viewer.js
@@ -0,0 +1,52 @@
+/* global smplr */
+import React, { memo } from 'react'
+import PropTypes from 'prop-types'
+
+import useSmplrJs from '../../../hooks/useSmplrJs'
+
+const Viewer = memo(({ mode, onReady, onModeChange }) => {
+  useSmplrJs({ onLoad })
+
+  function onLoad () {
+    const space = new smplr.Space({
+      spaceId: 'edb2ebaa-47ea-4e54-af0d-cf543328bdb0',
+      spaceToken: 'X',
+      containerId: 'smplr-container'
+    })
+    space.startViewer({
+      preview: true,
+      mode,
+      allowModeChange: true,
+      onModeChange,
+      onReady: () => onReady(space),
+      onError: error => console.error('Could not start viewer', error)
+    })
+  }
+
+  return (
+    <div
+      style={{
+        position: 'relative',
+        paddingBottom: '60%'
+      }}
+    >
+      <div
+        id='smplr-container'
+        style={{
+          position: 'absolute',
+          width: '100%',
+          height: '100%',
+          backgroundColor: 'whitesmoke'
+        }}
+      />
+    </div>
+  )
+})
+
+Viewer.propTypes = {
+  mode: PropTypes.string.isRequired,
+  onReady: PropTypes.func.isRequired,
+  onModeChange: PropTypes.func
+}
+
+export default Viewer

04355627eca1b3a9c35374afc0ab4f7d6c592dd1
nathanielrenninger <108883707+nathanielrenninger@users.noreply.github.com>
2022-07-07 22:45:51 -0400

    update

--- simplrspace2/Viewer.js
+++ simplrspace2/Viewer.js
@@ -1,26 +1,28 @@
-import React, { memo } from 'react'
-	import PropTypes from 'prop-types'
-  import useSmplrJs from './hooks/useSmplrJs'
 
-	const Viewer = memo(({ mode, onReady, onModeChange }) => {
+  import React, { memo } from 'react'
+import PropTypes from 'prop-types'
+
+import useSmplrJs from '../../../hooks/useSmplrJs'
+
+const Viewer = memo(({ mode, onReady, onModeChange }) => {
   useSmplrJs({ onLoad })
 
-	  function onLoad () {
-	    const space = new smplr.Space({
-	      spaceId: 'a3a34437-b393-4666-8911-aaaad38926d0', // prod
-	      // spaceId: '96eae952-ef60-4058-aba1-6ace322506e7', // dev
-	      spaceToken: 'X',
-	      containerId: 'smplr-container'
-	    })
-	    space.startViewer({
-	      preview: true,
-	     mode,
+  function onLoad () {
+    const space = new smplr.Space({
+      spaceId: 'a3a34437-b393-4666-8911-aaaad38926d0',
+      spaceToken: 'X',
+      containerId: 'smplr-container'
+    })
+    space.startViewer({
+      preview: true,
+      mode,
       allowModeChange: true,
       onModeChange,
       onReady: () => onReady(space),
       onError: error => console.error('Could not start viewer', error)
-	    })
-	  }
+    })
+  }
+
   return (
     <div
       style={{
